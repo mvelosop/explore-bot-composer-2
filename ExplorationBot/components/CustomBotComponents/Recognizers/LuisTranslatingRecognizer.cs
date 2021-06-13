fd7e3bcb582ab3bff35bd18b6b3ac08130f9961a
Miguel Veloso <mvelosop@gmail.com>
2021-06-13 19:16:15 +0100

    Add initial component project

---
+++ ExplorationBot/components/CustomBotComponents/Recognizers/LuisTranslatingRecognizer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Bot.Builder.AI.Luis;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomBotComponents.Recognizers
+{
+    public class LuisTranslatingRecognizer : LuisAdaptiveRecognizer
+    {
+        [JsonProperty("$kind")]
+        public new const string Kind = "Custom.LuisRecognizer";
+    }
+}
