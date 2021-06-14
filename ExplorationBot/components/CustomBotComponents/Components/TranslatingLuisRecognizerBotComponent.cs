0d7642c6a892229e9e13e2e99b70d38493081677
Miguel Veloso <mvelosop@gmail.com>
2021-06-14 22:04:02 +0100

    Add custom recognizer component

---
+++ ExplorationBot/components/CustomBotComponents/Components/TranslatingLuisRecognizerBotComponent.cs
@@ -0,0 +1,17 @@
+﻿using CustomBotComponents.Recognizers;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomBotComponents.Components
+{
+    public class TranslatingLuisRecognizerBotComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<DeclarativeType>(
+                sp => new DeclarativeType<TranslatingLuisRecognizer>(TranslatingLuisRecognizer.Kind));
+        }
+    }
+}
---
+++ ExplorationBot/components/CustomBotComponents/Recognizers/TranslatingLuisRecognizer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomBotComponents.Recognizers
+{
+    public class TranslatingLuisRecognizer : LuisAdaptiveRecognizer
+    {
+        [JsonProperty("$kind")]
+        public new const string Kind = "Custom.TranslatingLuisRecognizer";
+
+        public override Task<RecognizerResult> RecognizeAsync(DialogContext dialogContext, Activity activity, CancellationToken cancellationToken = default, Dictionary<string, string> telemetryProperties = null, Dictionary<string, double> telemetryMetrics = null)
+        {
+            Log.Information("----- Recognizing (LUIS) '{Text}' [{locale}] ({@Activity})", activity.Text, activity.Locale, activity);
+
+            return base.RecognizeAsync(dialogContext, activity, cancellationToken, telemetryProperties, telemetryMetrics);
+        }
+    }
+}
